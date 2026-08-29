f8168c274d6dd3638e895da69bf195b0f218b944
kaczorkacper28 <kaczorkacper28@gmail.com>
2026-08-29 23:47:11 +0200

    Napraw awaryjna obsluge showModal

--- modal-fix.js
+++ modal-fix.js
@@ -1,28 +1,36 @@
-const { ButtonInteraction, Routes } = require('discord.js');
+const discord = require('discord.js');
+const { Routes } = discord;
 
-// Awaryjna obsługa modali. Jeśli użyta wersja/runtime discord.js
-// nie udostępnia interaction.showModal(), wysyłamy callback typu 9
-// bezpośrednio do Discord API.
-if (typeof ButtonInteraction.prototype.showModal !== 'function') {
-  ButtonInteraction.prototype.showModal = async function (modal) {
-    if (!this.client?.rest) {
-      throw new Error('Discord REST nie jest dostępny.');
-    }
+// Kompatybilność: niektóre środowiska mogą nie udostępniać
+// interaction.showModal(). W takim przypadku wysyłamy callback typu 9
+// bezpośrednio przez Discord REST API.
+async function showModalCompat(modal) {
+  if (!this.client?.rest) {
+    throw new Error('Discord REST nie jest dostępny.');
+  }
 
-    const data = typeof modal?.toJSON === 'function' ? modal.toJSON() : modal;
+  const data = typeof modal?.toJSON === 'function' ? modal.toJSON() : modal;
 
-    await this.client.rest.post(
-      Routes.interactionCallback(this.id, this.token),
-      {
-        body: {
-          type: 9,
-          data
-        }
+  return this.client.rest.post(
+    Routes.interactionCallback(this.id, this.token),
+    {
+      body: {
+        type: 9,
+        data
       }
-    );
-  };
-
-  console.log('GROM: włączono awaryjny moduł showModal().');
+    }
+  );
 }
+
+// Ustawiamy metodę bezwarunkowo na kilku poziomach prototypu.
+// Dzięki temu działa także wtedy, gdy konkretna klasa interakcji
+// nie ma własnej implementacji showModal().
+for (const Type of [discord.Interaction, discord.BaseInteraction, discord.ButtonInteraction]) {
+  if (Type?.prototype) {
+    Type.prototype.showModal = showModalCompat;
+  }
+}
+
+console.log('GROM: kompatybilna obsługa showModal aktywna.');
 
 require('./index.js');
